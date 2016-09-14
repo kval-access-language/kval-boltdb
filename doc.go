b4880efba99357eb9b46375f6ac2cc9efbd1a8dc
ross-spencer <allalongthewatchtower2001@gmail.com>
2016-09-14 13:12:45 +1200

    [documentation] Thinking about Godoc.

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Package that will contain a BoltDB binding for the KVAL - Key Value Access Language
+The entry point will be converted to unit tests once the basic functions are up and
+running. Main will become package kvalboltdb.
+*/
+
+package main
